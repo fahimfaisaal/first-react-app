ff29ac6ce93604112ebbb592aeb104232395ea02
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-05-03 12:10:27 +0600

    props.children component

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,9 +3,10 @@ import { Component } from 'react';
 class Child extends Component {
     render() {
         this.props.func(this);
-        
+
         return <div className="children">
             <h2>I am children heading!</h2>
+            {this.props.children}
         </div>
     }
 }
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -12,7 +12,10 @@ class Parent extends Component {
 
         return <div className="parent">
             <h1>Hello i am parent Heading!</h1>
-            <Child func={this.getContext} />
+            <Child func={this.getContext}>
+                <h3>Hello i am from Parent</h3>
+                <h4>But i am a children of child</h4>
+            </Child>
         </div>
     }
 }
