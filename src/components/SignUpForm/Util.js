7774470bf5d68eaf23507d4f81c84722fa86b123
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-05-30 23:54:06 +0600

    feat: add some new utitlity funciton

--- src/components/SignUpForm/Util.js
+++ src/components/SignUpForm/Util.js
@@ -1,4 +1,12 @@
 class Util {
+    static $(el) {
+        return document.querySelector(el);
+    }
+
+    static $$(el) {
+        return [...document.querySelectorAll(el)];
+    }
+
     static CSS(selector, styles) {
         for (let key in styles) {
             selector.style[key] = styles[key];
@@ -14,11 +22,25 @@ class Util {
     }
 
     static trueFalse(expression) {
+        if (expression === '0') {
+            expression = 1
+        }
+
         if (expression) {
             return true;
         }
 
         return false;
+    }
+
+    static splitString(str) {
+        return {
+            length: str.length,
+            uppercase: str.replace(/[^A-Z]/g, ''),
+            lowercase: str.replace(/[^a-z]/g, ''),
+            digit: str.replace(/[\D]/g, ''),
+            specialChar: str.replace(/[^\W_]/g, '')
+        }
     }
 }
 
